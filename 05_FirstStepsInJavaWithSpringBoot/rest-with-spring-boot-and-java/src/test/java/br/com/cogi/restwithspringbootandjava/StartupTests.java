a7e27b2a630a839ed2583f925669bc91e4fc8afd
Gabriel Henrique dos Santos <gabriel.santos@meisters.solutions>
2024-03-21 10:46:35 -0300

    First Steps in Java with Spring Boot

---
+++ 05_FirstStepsInJavaWithSpringBoot/rest-with-spring-boot-and-java/src/test/java/br/com/cogi/restwithspringbootandjava/StartupTests.java
@@ -0,0 +1,13 @@
+package br.com.cogi.restwithspringbootandjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartupTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
